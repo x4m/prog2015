b1b6ca0a8e1f5844cfb2a98234c7b05eac27386e
Danil328 <Danil328@yandex.ru>
2015-03-30 18:19:06 +0500

    А3

---
+++ А3/Sportsmens/Sportsmens/RegistrationReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Registration
+{
+    public partial class RegistrationReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public RegistrationReport()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
