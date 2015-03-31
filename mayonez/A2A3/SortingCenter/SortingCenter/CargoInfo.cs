1cc294bf4d80690b821c3a06cef846a910b69ad4
mayonez <lalky@mail.ru>
2015-03-31 22:56:51 +0500

    mayonez A2A3

---
+++ mayonez/A2A3/SortingCenter/SortingCenter/CargoInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SortingCenter
+{
+    public partial class CargoInfo : Form
+    {
+        public CargoInfo()
+        {
+            InitializeComponent();
+        }
+
+        public InsideData Meow { get; set; }
+
+      private void button1_Click(object sender, EventArgs e)
+        {
+          Meow = new InsideData {Name = textbox1.Text, Weight = textbox2.Text, Words = textbox3.Text};
+        }
+
+      private void button2_Click(object sender, EventArgs e)
+      {
+          Close();
+      }
+
+
+    }
+}
