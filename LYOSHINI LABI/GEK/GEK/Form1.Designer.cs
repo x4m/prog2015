1c3d92a1b24aa3e9ad7469ac9db72f5b0901bfdf
Mikhaylov Alexey <dssen@ya.ru>
2015-04-04 00:41:26 +0500

    A2
    Вроде даже работает

---
+++ LYOSHINI LABI/GEK/GEK/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace GEK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBSName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBPatr = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBAdr = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DTPDate = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BAddItem = new System.Windows.Forms.Button();
+            this.BRemoveItem = new System.Windows.Forms.Button();
+            this.BSave = new System.Windows.Forms.Button();
+            this.BLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фамилия";
+            // 
+            // TBSName
+            // 
+            this.TBSName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBSName.Location = new System.Drawing.Point(15, 29);
+            this.TBSName.Name = "TBSName";
+            this.TBSName.Size = new System.Drawing.Size(410, 20);
+            this.TBSName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Имя";
+            // 
+            // TBName
+            // 
+            this.TBName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBName.Location = new System.Drawing.Point(15, 72);
+            this.TBName.Name = "TBName";
+            this.TBName.Size = new System.Drawing.Size(410, 20);
+            this.TBName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Отчество";
+            // 
+            // TBPatr
+            // 
+            this.TBPatr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBPatr.Location = new System.Drawing.Point(12, 115);
+            this.TBPatr.Name = "TBPatr";
+            this.TBPatr.Size = new System.Drawing.Size(410, 20);
+            this.TBPatr.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Адрес";
+            // 
+            // TBAdr
+            // 
+            this.TBAdr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBAdr.Location = new System.Drawing.Point(12, 183);
+            this.TBAdr.Name = "TBAdr";
+            this.TBAdr.Size = new System.Drawing.Size(410, 20);
+            this.TBAdr.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Дата";
+            // 
+            // DTPDate
+            // 
+            this.DTPDate.Location = new System.Drawing.Point(15, 227);
+            this.DTPDate.Name = "DTPDate";
+            this.DTPDate.Size = new System.Drawing.Size(133, 20);
+            this.DTPDate.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 292);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(407, 186);
+            this.listBox1.TabIndex = 50;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Счётчики";
+            // 
+            // BAddItem
+            // 
+            this.BAddItem.Location = new System.Drawing.Point(15, 484);
+            this.BAddItem.Name = "BAddItem";
+            this.BAddItem.Size = new System.Drawing.Size(82, 25);
+            this.BAddItem.TabIndex = 6;
+            this.BAddItem.Text = "Добавить";
+            this.BAddItem.UseVisualStyleBackColor = true;
+            this.BAddItem.Click += new System.EventHandler(this.BAddItem_Click);
+            // 
+            // BRemoveItem
+            // 
+            this.BRemoveItem.Location = new System.Drawing.Point(103, 484);
+            this.BRemoveItem.Name = "BRemoveItem";
+            this.BRemoveItem.Size = new System.Drawing.Size(82, 25);
+            this.BRemoveItem.TabIndex = 7;
+            this.BRemoveItem.Text = "Убрать";
+            this.BRemoveItem.UseVisualStyleBackColor = true;
+            this.BRemoveItem.Click += new System.EventHandler(this.BRemoveItem_Click);
+            // 
+            // BSave
+            // 
+            this.BSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BSave.Location = new System.Drawing.Point(255, 552);
+            this.BSave.Name = "BSave";
+            this.BSave.Size = new System.Drawing.Size(82, 25);
+            this.BSave.TabIndex = 8;
+            this.BSave.Text = "Сохранить";
+            this.BSave.UseVisualStyleBackColor = true;
+            this.BSave.Click += new System.EventHandler(this.BSave_Click);
+            // 
+            // BLoad
+            // 
+            this.BLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BLoad.Location = new System.Drawing.Point(343, 552);
+            this.BLoad.Name = "BLoad";
+            this.BLoad.Size = new System.Drawing.Size(82, 25);
+            this.BLoad.TabIndex = 9;
+            this.BLoad.Text = "Загрузить";
+            this.BLoad.UseVisualStyleBackColor = true;
+            this.BLoad.Click += new System.EventHandler(this.BLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 589);
+            this.Controls.Add(this.BLoad);
+            this.Controls.Add(this.BRemoveItem);
+            this.Controls.Add(this.BSave);
+            this.Controls.Add(this.BAddItem);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.DTPDate);
+            this.Controls.Add(this.TBAdr);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TBPatr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TBName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TBSName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Запись показаний счётчиков";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBSName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBPatr;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBAdr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker DTPDate;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BAddItem;
+        private System.Windows.Forms.Button BRemoveItem;
+        private System.Windows.Forms.Button BSave;
+        private System.Windows.Forms.Button BLoad;
+    }
+}
+
