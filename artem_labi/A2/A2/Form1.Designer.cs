86ec762ff0a607590397ff0294c0357dac390792
Артем Норицин <noritzin@yandex.ru>
2015-04-18 20:28:10 +0500

    A2
    A2

---
+++ artem_labi/A2/A2/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace ws
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Text_Box_Surname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Text_Box_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Text_Box_Patronymic = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Date_Birthday = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AddItem = new System.Windows.Forms.Button();
+            this.RemoveItem = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Фамилия";
+            // 
+            // Text_Box_Surname
+            // 
+            this.Text_Box_Surname.Location = new System.Drawing.Point(12, 39);
+            this.Text_Box_Surname.Name = "Text_Box_Surname";
+            this.Text_Box_Surname.Size = new System.Drawing.Size(118, 20);
+            this.Text_Box_Surname.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // Text_Box_name
+            // 
+            this.Text_Box_name.Location = new System.Drawing.Point(12, 95);
+            this.Text_Box_name.Name = "Text_Box_name";
+            this.Text_Box_name.Size = new System.Drawing.Size(118, 20);
+            this.Text_Box_name.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Отчество";
+            // 
+            // Text_Box_Patronymic
+            // 
+            this.Text_Box_Patronymic.Location = new System.Drawing.Point(12, 152);
+            this.Text_Box_Patronymic.Name = "Text_Box_Patronymic";
+            this.Text_Box_Patronymic.Size = new System.Drawing.Size(121, 20);
+            this.Text_Box_Patronymic.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Location = new System.Drawing.Point(12, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 53;
+            this.label4.Text = "Амплуа";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Вратарь",
+            "Защитник",
+            "Полузащитник",
+            "Нападающий"});
+            this.comboBox1.Location = new System.Drawing.Point(9, 201);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 54;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 55;
+            this.label7.Text = "Рабочая нога";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Правая",
+            "Левая"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 258);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 56;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 296);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 17);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "Дата рождения";
+            // 
+            // Date_Birthday
+            // 
+            this.Date_Birthday.Location = new System.Drawing.Point(12, 316);
+            this.Date_Birthday.Name = "Date_Birthday";
+            this.Date_Birthday.Size = new System.Drawing.Size(121, 20);
+            this.Date_Birthday.TabIndex = 58;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(172, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 17);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "Бывшие клубы";
+            // 
+            // AddItem
+            // 
+            this.AddItem.Location = new System.Drawing.Point(175, 261);
+            this.AddItem.Name = "AddItem";
+            this.AddItem.Size = new System.Drawing.Size(82, 25);
+            this.AddItem.TabIndex = 61;
+            this.AddItem.Text = "Добавить";
+            this.AddItem.UseVisualStyleBackColor = true;
+            this.AddItem.Click += new System.EventHandler(this.AddItem_Click);
+            // 
+            // RemoveItem
+            // 
+            this.RemoveItem.Location = new System.Drawing.Point(263, 261);
+            this.RemoveItem.Name = "RemoveItem";
+            this.RemoveItem.Size = new System.Drawing.Size(82, 25);
+            this.RemoveItem.TabIndex = 62;
+            this.RemoveItem.Text = "Убрать";
+            this.RemoveItem.UseVisualStyleBackColor = true;
+            this.RemoveItem.Click += new System.EventHandler(this.RemoveItem_Click);
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save.Location = new System.Drawing.Point(380, 347);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(82, 25);
+            this.Save.TabIndex = 63;
+            this.Save.Text = "Сохранить";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Load
+            // 
+            this.Load.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Load.Location = new System.Drawing.Point(468, 347);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(82, 25);
+            this.Load.TabIndex = 64;
+            this.Load.Text = "Загрузить";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(183, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(367, 147);
+            this.listBox1.TabIndex = 65;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 384);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.RemoveItem);
+            this.Controls.Add(this.AddItem);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Date_Birthday);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Text_Box_Patronymic);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Text_Box_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Text_Box_Surname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Резюме футблиста";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Text_Box_Surname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Text_Box_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Text_Box_Patronymic;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker Date_Birthday;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button AddItem;
+        private System.Windows.Forms.Button RemoveItem;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
