a4098defe31c04b02ecf811068cf5d6138b14256
DaryaKurochkina <d.curochckina@yandex.ru>
2015-05-16 15:51:45 +0500

    A4, C1

---
+++ DaryaKurochkina/A4_C1/A2/A2/Blank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace A2
+{
+    public partial class Blank : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Blank()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ DaryaKurochkina/A4_C1/A2/MvcApplication1/Controllers/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    public class Event
+    {
+       
+
+            public Event()
+            {
+                Form2 = new List<People>();
+            }
+
+            public DateTime dateTimePicker1 { get; set; }
+            public string Time { get; set; }
+            public string textBox6 { get; set; }
+            public string textBox5 { get; set; }
+            public string textBox4 { get; set; }
+            public string textBox3 { get; set; }
+            public List<People> Form2 { get; set; }
+        }
+
+        public class People
+        {
+            public string textBox1 { get; set; }
+            public string textBox2 { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("Фимилия ИО: {0}; Контактный телефон: {1}", textBox1, textBox2);
+            }
+        }
+}
