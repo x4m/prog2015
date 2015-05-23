ae3019dd6bc684a25e2243cdfce6d73b309fa2f0
Артем Норицин <noritzin@yandex.ru>
2015-05-23 20:52:18 +0500

    с1

---
+++ artem_labi/C1(угадчисло)/C1(угадчисло)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1_угадчисло_
+{
+    class Program
+    {
+        static int maxchislo = 101;
+        static int minchislo = 0;
+        static int srednee = ((maxchislo + minchislo)/2);
+        static bool otgadano = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Загадайте число от 0 до 100 (Чтобы подтвердить пишите yes)");
+            while (!otgadano)
+            {
+                Console.WriteLine("Ваше число меньше " + srednee + "?");
+                if (Console.ReadLine().ToLower() == "yes")
+                    maxchislo = srednee;
+                else 
+                    minchislo = srednee;
+                srednee = ((maxchislo + minchislo) / 2);
+                if (srednee == minchislo)
+                    break;
+            }
+                
+               Console.WriteLine("Вы загадывли число " + minchislo + "!");
+               Console.ReadKey();
+          }
+        }
+    }
+
---
+++ artem_labi/C1(угадчисло)/C1(угадчисло)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("C1(угадчисло)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("C1(угадчисло)")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("92a28fd9-0c72-462a-aab7-f435c0eea8eb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
