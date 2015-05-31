bad0d51cf80ae810f759cdc5c694a20c78a3aa30
ti-rex <tiranno-zavr@mail.ru>
2015-05-31 19:28:35 +0600

    All labs

---
+++ Labs/A1/Laba1/Cs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    public class Cs
+    {
+        
+
+        public int Power { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public Aura Type { get; set; }
+
+  
+    }
+
+    public enum Aura
+    {
+        green,
+        dark,
+        blue,
+        orange,
+        white,
+        gold
+    }
+}
