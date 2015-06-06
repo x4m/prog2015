fa18f3d53689cfee1eb65adb131c582051c51605
VictoriaGlushkova <Victoria_GL_RTF@mail.ru>
2015-06-06 22:18:02 +0600

    A2, A3, D1

---
+++ VictoriaGlushkova/A2/The Voice/SingerData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Voice
+{
+    public class SingerData
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Region { get; set; }
+        public string Hometown { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Information { get; set; }
+        public byte[] Photo { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
