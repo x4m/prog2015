11ab05f4122561e9eec253a27726e20135b0564f
hodenlalala <hoden.lalala@gmail.com>
2015-06-15 16:05:15 +0500

    A2-3, C1, D1

---
+++ SkyLine/C1/ConsoleApplication2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static int max = 1000000;
+        private static int maxlimit=max;
+        static int minlimit=0;
+        static int find = max / 2;
+        static bool success = false;
+        static void Main(string[] args)
+        {
+            bool need = false;
+            Console.WriteLine("Загадайте число от 0 до {0}",max);
+            while (!need)
+            {
+                Console.WriteLine("Загадали?");
+                if (Console.ReadLine().ToLower() == "да")
+                    need = true;
+            }
+            while (!success)
+            {
+                Vopros();
+                Computer();
+            }
+        }
+        private static void Computer()
+        {
+            find = (int)((maxlimit + minlimit)/2);
+        }
+
+        private static void Vopros()
+        {
+            Console.WriteLine("Ваше число " + find.ToString() + "?");
+            if (Console.ReadLine().ToLower() == "да")
+            {
+                success = true;               
+            }     
+            else
+            {
+                Console.WriteLine("Ваше число больше чем "+find.ToString()+"?");
+                if (Console.ReadLine().ToLower() == "да")
+                {
+                    minlimit = find;                   
+                }
+                else maxlimit=find;
+
+            }
+        }
+    }
+}
